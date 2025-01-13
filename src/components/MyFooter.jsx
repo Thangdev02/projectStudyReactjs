869d16d77aced242251bed89d93530b03d908b77
Le Quoc Thang <thanglq@Thangs-Macbook.local>
2025-01-13 20:11:17 +0700

    footer

--- src/components/MyFooter.jsx
+++ src/components/MyFooter.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
-
+import Logo from '../assets/images/StarbuckLogo.webp'
+import '../styles/MyFooter.css'
 const MyFooter = () => {
     return (
-        <div>
-            
-        </div>
+        <footer className='footer'>
+            <div className='colum1'>
+                <img className='logoFooter' src={Logo} alt='Logo Starbuck' />
+            </div>
+            <div className='colum2'>
+                <h1>Menu</h1>
+                <ul>
+                    <li>Home</li>
+                    <li>Contact</li>
+                    <li>Profile</li>
+                    <li>AboutUS</li>
+                </ul>
+            </div>
+            <div className='colum3'>
+                <h1>Menu2</h1>
+                <ul>
+                    <li>FAQ</li>
+                    <li>Message</li>
+                    <li>Quizz</li>
+                    <li>Test</li>
+                </ul>
+            </div>
+            <div className='colum4'>
+                <h1>Menu3</h1>
+                <ul>
+                    <li>ShowCase</li>
+                    <li>Support</li>
+                    <li>Service</li>
+                </ul>
+            </div>
+        </footer>
     );
 };
 
--- src/components/Myheader.jsx
+++ src/components/Myheader.jsx
@@ -2,20 +2,20 @@ import React from 'react';
 import '../styles/MyHeader.css'
 const Myheader = () => {
     return (
-        <header>
+        <header className='header'>
             <div className='logo'>
                 <img alt='Logo Starbuck' src="https://www.saokim.com.vn/blog/wp-content/uploads/2022/04/logo-moi-cua-starbucks.jpg.webp" />
             </div>
-            <nav>
-                <ul>
-                    <a href='#'> <li>Home</li></a>
+            <nav className='nav'>
+                <ul className='ul'>
+                    <a href='/#'> <li>Home</li></a>
                     <a href='/contact'> <li>Contact</li></a>
                     <a href='/profile'> <li>Profile</li></a>
                     <a href='/aboutUs'> <li>AboutUS</li></a>
                 </ul>
             </nav>
             <div className='btn'>
-                <button>Login</button>
+                <button className='button'>Login</button>
             </div>
         </header>
     );
